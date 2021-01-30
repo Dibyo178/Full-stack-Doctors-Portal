eb734841775d4baa9ee58ebd6d3ee02ba89f0618
Dibyo178 <Sourov.mu@gmail.com>
2021-01-30 23:34:34 +0600

    frontend finish

--- src/components/Home/ServiceDetail/ServiceDetail.js
+++ src/components/Home/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './ServiceDetails.css';
 const ServiceDetail = ({ service }) => {
     return (
-        <div className='col-md-4 text-center'>
+        <div className='col-md-4 text-center single-item'>
             <img style={{height:'50px'}} src={service.img} alt="" />
             <h5 className='mt-3 mb-3'>{service.name}</h5>
-            <p className='text-secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, corporis?</p>
+            <p id='p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, corporis?</p>
 
         </div>
     );
